dc4994df655ad1f677d201d6b8dc619f9880a2c8
Tranvanloc-2003 <tranvanlocwork@gmail.com>
2025-09-17 15:09:44 +0700

    insert and update data

--- manager_lesson/includes/database.php
+++ manager_lesson/includes/database.php
@@ -5,19 +5,71 @@ if (!defined('BASE_URL')) {
     die('Bạn không có quyền truy cập vào file này');
 }
 //truy vấn nhiều dòng dl
-function getAll($sql){
+function getAll($sql)
+{
     global $conn;
-        $stm = $conn -> prepare($sql);
-        $stm -> execute();
-        $result = $stm -> fetchAll(PDO::FETCH_ASSOC);
-        return $result;
+    $stm = $conn->prepare($sql);
+    $stm->execute();
+    $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
 }
 
 //truy vấn 1 dòng dl 
-function getOne($sql){
+function getOne($sql)
+{
     global $conn;
-        $stm = $conn -> prepare($sql);
-        $stm -> execute();
-        $result = $stm -> fetch(PDO::FETCH_ASSOC);
-        return $result;
+    $stm = $conn->prepare($sql);
+    $stm->execute();
+    $result = $stm->fetch(PDO::FETCH_ASSOC);
+    return $result;
 }
+
+//insert dữ liệu
+
+function insertData($table, $data)
+{
+    /**    
+     * 
+     * $data = [
+     *   'name' => 'Nguyen Van A',
+     *   'email' => 'nguyenvana@example.com',
+     *   'phone' => '0123456789'
+     * ]
+     */
+    global $conn;
+
+    $keys = array_keys($data); //lấy ra các key của mảng
+    // echo $keys;
+    $cot = implode(',', $keys); //chuyển mảng thành chuỗi
+    $place = ':'. implode(',:', $keys);
+    echo $cot;
+    echo $place;
+    $sql = "INSERT INTO $table ($cot) VALUES($place)";
+    echo $sql;
+    $stm = $conn->prepare($sql); //SQL injection
+    // $name = "Nguyen Van A";
+    // $stm -> execute([':name' => $name]);
+    $stm->execute($data);
+
+
+}
+
+function updateData($table, $data, $condition='')
+{
+    global $conn;
+    $update="";
+    foreach($data as $key => $value){
+        $update .= $key. "=:" . $key.',';
+    }
+    $update = rtrim($update,',');
+  
+    if(!empty($condition)){
+        $sql = "UPDATE $table SET  $update WHERE $condition";
+    }else{
+        $sql = "UPDATE $table SET  $update";
+    } 
+     echo $sql;
+        $tmp = $conn->prepare($sql);
+         $tmp -> execute($data);
+}
+  
--- manager_lesson/index.php
+++ manager_lesson/index.php
@@ -7,30 +7,39 @@ require_once 'includes/connect.php';
 require_once 'includes/database.php';
 $module = _MODULES;
 $action = _ACTIONS;
-$rel =  getAll("SELECT * FROM course");
 
-echo '<pre>';
-print_r($rel);
-echo '</pre>';
-if(!empty($_GET['module'])){
+$data = [
+    'name' => 'Marketing',
+    'slug' => 'marketing',
+];
+
+// insertData('course_category', $data);
+$condition = "id=1";
+updateData('course_category', $data, $condition);
+
+// $rel =  getAll("SELECT * FROM course");
+
+// echo '<pre>';
+// print_r($rel);
+// echo '</pre>';
+if (!empty($_GET['module'])) {
 
     $module = $_GET['module'];
 }
 
-if(!empty($_GET['action'])){
+if (!empty($_GET['action'])) {
     $action = $_GET['action'];
 }
 
 $path = 'modules/' . $module . '/' . $action . '.php';
 
 
-if(!empty($path)){
-    if(file_exists($path)){
+if (!empty($path)) {
+    if (file_exists($path)) {
         require_once $path;
-
-    }else{
+    } else {
         require_once 'modules/errors/404.php';
     }
-}else{
+} else {
     require_once 'modules/errors/500.php';
 }
